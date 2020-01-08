d06682e7b13a437495f0699390c683205dc2f049
Dominick Bruno <dbruno@unseen.is>
2020-01-08 16:47:22 -0500

    Added celeb seeds

--- data/seeds/celebs.js
+++ data/seeds/celebs.js
@@ -1,13 +1,18 @@
 
 exports.seed = function(knex) {
+
+  let stat1 = new Boolean(true);
+  let stat0 = new Boolean(false);
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('celebs').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('celebs').insert([
+        {name: 'Harrison Ford', imageUrl: 'rowValue1', isDead: stat0.toString()},
+        {name: 'Bruce Campbell', imageUrl: 'rowValue1', isDead: stat0.toString()},
+        {name: 'Desi Arnaz', imageUrl: 'rowValue1', isDead: stat1.toString()},
+        {name: 'Merv Griffin', imageUrl: 'rowValue1', isDead: stat1.toString()},
+        {name: 'Nichelle Nichols', imageUrl: 'rowValue1', isDead: stat0.toString()}
       ]);
     });
 };
