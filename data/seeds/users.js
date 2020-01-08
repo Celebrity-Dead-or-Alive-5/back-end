5a86d9155a803f2f7c8e92cfdf61b2df83070cbd
Dominick Bruno <dbruno@unseen.is>
2020-01-07 23:33:13 -0500

    added user seeds

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -1,13 +1,15 @@
+const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {username: 'Brian', password: bcrypt.hashSync('oxford', 14)},
+        {username: 'Dom', password: bcrypt.hashSync('knox', 14)},
+        {username: 'Lee', password: bcrypt.hashSync('omen', 14)},
+        {username: 'Anthony', password: bcrypt.hashSync('nonic', 14)}
       ]);
     });
 };
